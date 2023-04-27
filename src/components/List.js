fe8fa7e90c1e67d6d37a40243a36e0cfb116ce6f
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-04-27 11:56:51 +0300

    complete mvp

--- src/components/List.js
+++ src/components/List.js
@@ -1,3 +1,4 @@
+import {AiOutlineCheck} from 'react-icons/ai';
 function List({ todos, setTodos }) {
   const incompleteTasks = todos.filter((task) => task.status === "incomplete");
   const completeTasks = todos.filter((task) => task.status === "complete");
@@ -9,8 +10,10 @@ function List({ todos, setTodos }) {
           Pending Tasks
         </h3>
         <hr className="border-gray" />
-        <ul className="divide-y divide-gray-100">
+        {incompleteTasks.length === 0 && <p className=" m-10 text-sm font-semibold leading-5 text-gray-500">🎉 All Tasks Completed!</p>}
+        <ul className="flex md:flex-cols-2 justify-center divide-y divide-gray-100">
           {incompleteTasks.map((task) => (
+            <>
             <li key={task.title} className="flex justify-between gap-x-6 py-5">
               <div className="flex gap-x-4">
                 <input
@@ -39,7 +42,7 @@ function List({ todos, setTodos }) {
                   </p>
                 </div>
               </div>
-            </li>
+            </li></>
           ))}
         </ul>
       </div>
@@ -48,8 +51,10 @@ function List({ todos, setTodos }) {
           Complete Tasks
         </h3>
         <hr className="border-gray" />
-        <ul className="divide-y divide-gray-100">
+        <ul className="flex md:flex-cols-2 justify-center gap-6 divide-y divide-gray-100">
           {completeTasks.map((task) => (
+            <>
+            <AiOutlineCheck className='mt-10' />
             <li key={task.title} className="flex justify-between gap-x-6 py-5">
               <div className="flex gap-x-4">
                 <div className="min-w-0 flex-auto">
@@ -65,6 +70,7 @@ function List({ todos, setTodos }) {
                 </div>
               </div>
             </li>
+            </>
           ))}
         </ul>
       </div>
